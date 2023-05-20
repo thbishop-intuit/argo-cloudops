afb9a4a4d5cbb382338c96a9522fe54723c3eae7
Tommy Bishop <thomas_bishop@intuit.com>
2023-05-19 20:12:06 -0700

    more vault provider plugin tests migrated

--- service/plugin/vault/provider_test.go
+++ service/plugin/vault/provider_test.go
@@ -148,6 +148,121 @@ func TestVaultCreateTarget(t *testing.T) {
 	}
 }
 
+func TestGetTarget(t *testing.T) {
+	tests := []struct {
+		name      string
+		admin     bool
+		vaultErr  error
+		errResult bool
+	}{
+		{
+			name:  "get target success",
+			admin: true,
+		},
+		{
+			name:      "get target admin error",
+			admin:     false,
+			errResult: true,
+		},
+		{
+			name:      "get target error",
+			admin:     true,
+			vaultErr:  errTest,
+			errResult: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			role := TestRole
+			if tt.admin {
+				role = authorizationKeyAdmin
+			}
+
+			v := VaultProvider{
+				roleID: role,
+				vaultSvcFn: mockVaultSvc(vaultSvc{
+					roleID: role,
+					vaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr, data: map[string]interface{}{
+						"role_arns":       []interface{}{"test-role-arn"},
+						"policy_arns":     []interface{}{"test-policy-arn"},
+						"policy_document": `{ "Version": "2012-10-17", "Statement": [ { "Effect": "Allow", "Action": "s3:ListBuckets", "Resource": "*" } ] }`,
+						"credential_type": "test-cred-type",
+					}},
+					vaultSysSvc: &mockVaultSys{},
+				}),
+			}
+
+			_, err := v.GetTarget(credentials.GetTargetArgs{
+				ProjectName: "testProject",
+				TargetName:  "testTarget",
+			})
+			if err != nil {
+				if !tt.errResult {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.errResult {
+					t.Errorf("\nexpected error")
+				}
+			}
+		})
+	}
+}
+
+func TestVaultProjectExists(t *testing.T) {
+	tests := []struct {
+		name      string
+		exists    bool
+		vaultErr  error
+		expectErr bool
+	}{
+		{
+			name:   "get project success",
+			exists: true,
+		},
+		{
+			name:      "get project not found",
+			exists:    false,
+			vaultErr:  ErrNotFound,
+			expectErr: false,
+		},
+		{
+			name:      "vault error",
+			exists:    false,
+			vaultErr:  errTest,
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := VaultProvider{
+				vaultSvcFn: mockVaultSvc(vaultSvc{
+					vaultLogicalSvc: &mockVaultLogical{err: tt.vaultErr},
+				}),
+			}
+
+			resp, err := v.ProjectExists(credentials.ProjectExistsArgs{
+				ProjectName: "testProject",
+			})
+			if err != nil {
+				if !tt.expectErr {
+					t.Errorf("\ndid not expect error, got: %v", err)
+				}
+			} else {
+				if tt.expectErr {
+					t.Errorf("\nexpected error")
+				}
+
+				if !cmp.Equal(resp.Exists, tt.exists) {
+					t.Errorf("\nwant: %v\n got: %v", tt.exists, resp.Exists)
+				}
+			}
+		})
+	}
+}
+
 func mockVaultSvc(vSvc vaultSvc) func(auth credentials.Authorization, h http.Header) (vaultSvc, error) {
 	return func(auth credentials.Authorization, h http.Header) (vaultSvc, error) {
 		return vSvc, nil
